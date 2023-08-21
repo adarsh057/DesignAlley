1f54a911d278b3b5c688d4e9fde8adb36259d1d6
adarsh057 <51473499+adarsh057@users.noreply.github.com>
2023-08-21 15:31:23 +0530

    Add files via upload

---
+++ MyEntryRenderer.cs
@@ -0,0 +1,42 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using DesignAlley;
+using DesignAlley.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(MyEntry), typeof(MyEntryRenderer))]
+namespace DesignAlley.Droid
+{
+    public class MyEntryRenderer : EntryRenderer
+    {
+        public MyEntryRenderer(Context context ):base(context)
+        {
+
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                Control.Background = null;
+
+                var layoutParams = new MarginLayoutParams(Control.LayoutParameters);
+                layoutParams.SetMargins(0, 0, 0, 0);
+                LayoutParameters = layoutParams;
+                Control.LayoutParameters = layoutParams;
+                Control.SetPadding(0, 0, 0, 0);
+                SetPadding(0, 0, 0, 0);
+            }
+        }
+    }
+}
